093e1a1450263c88b3da620ec5d2aed0f9ac7df3
Charlton Ortega <charltonortega14@gmail.com>
2023-08-15 13:57:51 +0930

    enhanced color validaton to support named colors and hex codes

--- index.js
+++ index.js
@@ -2,12 +2,22 @@ const inquirer = require('inquirer'); // Import the inquirer
 const fs = require('fs'); // Import the file system
 const { Triangle, Circle, Square } = require('./lib/shapes'); // Import the shapes
 const open = require('open');// Import the open module
+const path = require('path');
+
+const namedColors = [
+    'aqua', 'black', 'blue', 'fuchsia', 'gray', 'green', 
+    'lime', 'maroon', 'navy', 'olive', 'purple', 'red', 
+    'silver', 'teal', 'white', 'yellow'
+];
 
 function isValidColor(input) {
     // Check if it's a valid hex code
     if (/^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$/.test(input)) return true;
-
-    return true;
+    
+    // Check if it's a valid named color
+    if (namedColors.includes(input.toLowerCase())) return true;
+    
+    return false;  // Invalid color
 }
 
 // Define the questions for Inquirer
@@ -41,11 +51,21 @@ const questions = [
         name: 'shapeColor',
         message: 'Enter the shape color (color name or hex code):',
         validate: input => isValidColor(input) || 'Please enter a valid color name or hex code!'
+    },
+    {
+        type: 'input',
+        name: 'filename',
+        message: 'Enter a name for the logo file (without extension):',
+        default: 'logo',
+        validate: input => input.length > 0 || 'Filename cannot be empty!'
     }
 ];
 
+// Main function using inquirer to prompt the user
 inquirer.prompt(questions).then(answers => {
     let shape;
+    
+    // Determine the shape based on user input
     switch (answers.shape) {
         case 'circle':
             shape = new Circle(answers.shapeColor);
@@ -58,7 +78,7 @@ inquirer.prompt(questions).then(answers => {
             break;
     }
 
-    // Generate the SVG content
+    // Generate the SVG content with the chosen shape and text
     const svgContent = `
     <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
         ${shape.render()}
@@ -68,10 +88,19 @@ inquirer.prompt(questions).then(answers => {
     </svg>
     `;
 
-    // Write the SVG content to a file
-    fs.writeFileSync('logo.svg', svgContent);
+    // Define the output path for the SVG file
+    const outputPath = path.join(__dirname, 'examples', `${answers.filename}.svg`);
 
-    console.log('logo.svg has been generated successfully!');
-    open('logo.svg'); // using open module the logo will open automatically after
+    // Check if a file with the provided name already exists and handle accordingly
+    if (fs.existsSync(outputPath)) {
+        console.error('A file with this name already exists. Choose a different name.');
+        return;
+    }
+    
+    // Write the SVG content to the file
+    fs.writeFileSync(outputPath, svgContent);
+    console.log(`Generated ${answers.filename}.svg in the examples folder.`);
+    
+    // Preview the generated SVG
+    open(outputPath);
 });
-
