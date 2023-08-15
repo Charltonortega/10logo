a3300a405c143692bc739af7c8abe5c61de69672
Charlton Ortega <charltonortega14@gmail.com>
2023-08-15 12:58:25 +0930

    Implemented CLI using Inquirer to generate logo.svg

--- index.js
+++ index.js
@@ -1,17 +1,60 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {triangle, Circle, Square} = require('./shapes');
+const { Triangle, Circle, Square } = require('./lib/shapes');
 
-const question = [
+// Define the questions for Inquirer
+const questions = [
     {
         type: 'input',
-        name: 'color',
-        message: 'Enter up to three characters for the logo text:'
+        name: 'text',
+        message: 'Enter up to three characters for the logo text:',
         validate: input => input.length <= 3 || 'Please enter up to three characters only!'
     },
     {
         type: 'input',
-        name: 'textColor'
-        message: 'Enter the text color (color name or hex code)'
+        name: 'textColor',
+        message: 'Enter the text color (color name or hex code):'
     },
-]:
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'Choose a shape:',
+        choices: ['circle', 'triangle', 'square']
+    },
+    {
+        type: 'input',
+        name: 'shapeColor',
+        message: 'Enter the shape color (color name or hex code):'
+    }
+];
+
+inquirer.prompt(questions).then(answers => {
+    let shape;
+    switch (answers.shape) {
+        case 'circle':
+            shape = new Circle(answers.shapeColor);
+            break;
+        case 'triangle':
+            shape = new Triangle(answers.shapeColor);
+            break;
+        case 'square':
+            shape = new Square(answers.shapeColor);
+            break;
+    }
+
+    // Generate the SVG content
+    const svgContent = `
+    <svg width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${shape.render()}
+        <text x="150" y="100" font-family="Arial" font-size="24" fill="${answers.textColor}" text-anchor="middle" dy=".3em">
+            ${answers.text}
+        </text>
+    </svg>
+    `;
+
+    // Write the SVG content to a file
+    fs.writeFileSync('logo.svg', svgContent);
+
+    console.log('Generated logo.svg');
+});
+
